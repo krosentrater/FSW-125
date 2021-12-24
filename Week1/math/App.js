1fa0e7fcc281cfab69e8fa175d137657308e769a
krosentrater <87054047+krosentrater@users.noreply.github.com>
2021-12-24 11:08:38 -0800

    Week 1 Assignment 1
    Math assignment for  imports/exports.

---
+++ Week1/math/App.js
@@ -0,0 +1,9 @@
+const math = require('./Math.js');
+
+console.log('Add: ' + math.addition(10, 22));
+
+console.log('Sub: ' + math.subtraction(64, 18));
+
+console.log('Mult: ' + math.multiplication(8, 8));
+
+console.log('Div: ' + math.division(10000, 100));
---
+++ Week1/math/Math.js
@@ -0,0 +1,7 @@
+module.exports.addition = (a, b) => a + b;
+
+module.exports.subtraction = (a, b) => a - b;
+
+module.exports.multiplication = (a, b) => a * b;
+
+module.exports.division = (a, b) => a / b;
